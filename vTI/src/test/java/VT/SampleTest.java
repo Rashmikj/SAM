d5f3744c4ab0de141d24dff8b90da6708d86530a
rashmi <rashmijagadeesh764@gmail.com>
2021-03-15 13:33:40 +0530

    FM

---
+++ vTI/src/test/java/VT/SampleTest.java
@@ -0,0 +1,15 @@
+package VT;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void contact() {
+		System.out.println("contact created");
+	}
+	@Test
+	public void contact1() {
+		System.out.println("contact1 created");
+	}
+
+}
